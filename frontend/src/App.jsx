e88692a6a1546ea6efd56bb9168f324129fcd2fe
Saket-db <dbsaket04@gmail.com>
2026-01-03 01:28:09 +0530

    Page Structure created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
+import { BrowserRoute, Route, Routes, Navigate } from 'react-router-dom'
+import Login from './pages/Login.jsx'
+import AdminDashboard from './pages/AdminDashboard.jsx'
 import './index.css'
 
 function App() {
 
+  // Navigate is used to redirect the route from one to other by making changes in the url.
   return (
-    <div className='text-3xl text-green-300'>Hello World</div>
+    <BrowserRoute>
+    <Routes>
+      <Route path = "/" element={<Navigate to = "/adminDashboard"></Navigate>}></Route> 
+      <Route path = "/login" element={<Login />}></Route>
+      <Route path = "/adminDashboard" element={<AdminDashboard />}></Route>
+    </Routes>
+    </BrowserRoute>
+
   )
 }
 
